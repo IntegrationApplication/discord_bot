3197937df5a9e6a82ae24343e58b83be7117a18c
drfailer <drfailer.42@gmail.com>
2024-01-07 18:43:37 +0100

    added utility functions for generating api url

---
+++ src/character_api.py
@@ -0,0 +1,41 @@
+CHARACTER_API = "https://localhost:7145/Character"
+CHARACTER_FRONT = "http://localhost:3000"
+
+################################################################################
+#                         url manipulations functions                          #
+################################################################################
+
+def createCharacterURL(idPlayer: int, idGame: int):
+    return f"{CHARACTER_API}/CreateCharacter?idPlayer={idPlayer}&idGame={idGame}"
+
+
+def getCharacterURL(idPlayer: int, idGame: int):
+    return f"{CHARACTER_API}/GetCharacter?idPlayer={idPlayer}&idGame={idGame}"
+
+
+def deleteCharacterURL(idPlayer: int, idGame: int):
+    return f"{CHARACTER_API}/DeleteCharacter?idPlayer={idPlayer}&idGame={idGame}"
+
+
+def takeDamageURL(idPlayer: int, idGame: int, amount: int):
+    return f"{CHARACTER_API}/DeleteCharacter?idPlayer={idPlayer}&idGame={idGame}&amount={amount}"
+
+
+def rollInitiativeURL(idPlayer: int, idGame: int):
+    return f"{CHARACTER_API}/RollInitiative?idPlayer={idPlayer}&idGame={idGame}"
+
+
+def rollAnyURL(idPlayer: int, idGame: int, abilityName: str):
+    return f"{CHARACTER_API}/RollAny?idPlayer={idPlayer}&idGame={idGame}&name={abilityName}"
+
+
+def rollAttackURL(idPlayer: int, idGame: int, weaponIdx: int):
+    return f"{CHARACTER_API}/RollAttack?idPlayer={idPlayer}&idGame={idGame}&index={weaponIdx}"
+
+
+def rollDamageURL(idPlayer: int, idGame: int, weaponIdx: int):
+    return f"{CHARACTER_API}/RollDamage?idPlayer={idPlayer}&idGame={idGame}&index={weaponIdx}"
+
+
+def modifyCharacterURL(idPlayer: int, idGame: int):
+    return f"{CHARACTER_FRONT}?idPlayer={idPlayer}&idGame={idGame}"
